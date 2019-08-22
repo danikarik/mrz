3a5b4ac629ebcb32e48feab5578c327a45a42476
danikarik <baitursynov92@gmail.com>
2019-08-22 23:39:15 +0600

    abs

--- cmd/detect/main.go
+++ cmd/detect/main.go
@@ -12,30 +12,28 @@ import (
 
 func main() {
 	rectKernel := gocv.GetStructuringElement(gocv.MorphRect, image.Point{X: 13, Y: 5})
-	// sqKernel := gocv.GetStructuringElement(gocv.MorphRect, image.Point{X: 21, Y: 21})
+	sqKernel := gocv.GetStructuringElement(gocv.MorphRect, image.Point{X: 21, Y: 21})
 
-	fname := "testdata/passport_01.jpg"
+	original := gocv.IMRead("testdata/passport_01.jpg", gocv.IMReadUnchanged)
+	dst := original.Clone()
 
-	mat := gocv.IMRead(fname, gocv.IMReadUnchanged)
-	mat = mrz.Resize(mat, 0, 600, gocv.InterpolationArea)
+	dst = mrz.Resize(dst, 0, 600, gocv.InterpolationArea)
+	dst = mrz.Color(dst, gocv.ColorBGRToGray)
 
-	gray := mrz.Color(mat, gocv.ColorBGRToGray)
-	gocv.GaussianBlur(gray, &gray, image.Point{X: 3, Y: 3}, 0, 0, gocv.BorderConstant)
+	gocv.GaussianBlur(dst, &dst, image.Point{X: 3, Y: 3}, 0, 0, gocv.BorderConstant)
+	gocv.MorphologyEx(dst, &dst, gocv.MorphBlackhat, rectKernel)
 
-	blackhat := gray.Clone()
-	gocv.MorphologyEx(gray, &blackhat, gocv.MorphBlackhat, rectKernel)
+	gocv.Sobel(dst, &dst, 0, 1, 0, 1, 1, 0, gocv.BorderConstant)
+	gocv.ConvertScaleAbs(dst, &dst, 1, 0)
 
-	// Python:
-	// gradX = cv2.Sobel(blackhat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
-	// gradX = np.absolute(gradX)
-	// (minVal, maxVal) = (np.min(gradX), np.max(gradX))
-	// gradX = (255 * ((gradX - minVal) / (maxVal - minVal))).astype("uint8")
+	gocv.MorphologyEx(dst, &dst, gocv.MorphClose, rectKernel)
+	gocv.Threshold(dst, &dst, 0, 255, gocv.ThresholdBinary|gocv.ThresholdOtsu)
 
-	// TODO:
-	gradX := blackhat.Clone()
-	gocv.Sobel(blackhat, &gradX, gocv.MatTypeCV32F, 1, 0, -1, 1, 0, gocv.BorderConstant)
+	gocv.MorphologyEx(dst, &dst, gocv.MorphClose, sqKernel)
+	m := gocv.NewMatWithSize(3, 3, gocv.MatTypeCV8U)
+	gocv.Erode(dst, &dst, m)
 
-	img, err := gradX.ToImage()
+	img, err := dst.ToImage()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -51,6 +49,6 @@ func main() {
 	}
 
 	window := gocv.NewWindow("Hello")
-	window.IMShow(mat)
+	window.IMShow(dst)
 	window.WaitKey(0)
 }
