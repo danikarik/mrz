c43cc38d5a333e63b25bd6e786016e1be68b5fb5
danikarik <baitursynov92@gmail.com>
2019-08-23 22:46:27 +0600

    remove test

--- mrz_test.go
+++ mrz_test.go
@@ -1,16 +1,11 @@
 package mrz_test
 
 import (
-	"bytes"
-	"image/png"
-	"io/ioutil"
-	"os"
 	"testing"
-
-	"gocv.io/x/gocv"
 
 	"github.com/danikarik/mrz"
 	"github.com/stretchr/testify/assert"
+	"gocv.io/x/gocv"
 )
 
 func TestVersion(t *testing.T) {
@@ -99,68 +94,6 @@ func TestResize(t *testing.T) {
 
 			assert.Equal(int(tc.ExpectedWidth), mat.Cols())
 			assert.Equal(int(tc.ExpectedHeight), mat.Rows())
-		})
-	}
-}
-
-func TestScanImageFromBytes(t *testing.T) {
-	testCases := []struct {
-		Name string
-		Path string
-	}{
-		{
-			Name: "Passport01",
-			Path: "testdata/passport_01.jpg",
-		},
-		{
-			Name: "Passport02",
-			Path: "testdata/passport_02.jpg",
-		},
-		{
-			Name: "Passport03",
-			Path: "testdata/passport_03.jpg",
-		},
-		{
-			Name: "Passport04",
-			Path: "testdata/passport_04.jpg",
-		},
-		{
-			Name: "Passport05",
-			Path: "testdata/passport_05.jpg",
-		},
-		{
-			Name: "Passport06",
-			Path: "testdata/passport_06.jpg",
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.Name, func(t *testing.T) {
-			assert := assert.New(t)
-
-			file, err := os.Open(tc.Path)
-			if !assert.NoError(err) {
-				return
-			}
-			defer file.Close()
-
-			data, err := ioutil.ReadAll(file)
-			if !assert.NoError(err) {
-				return
-			}
-
-			imageData, err := mrz.ScanImageFromBytes(data)
-			if !assert.NoError(err) {
-				return
-			}
-
-			img, err := png.Decode(bytes.NewReader(imageData))
-			if !assert.NoError(err) {
-				return
-			}
-
-			t.Log(img.Bounds().Size().X)
-			t.Log(img.Bounds().Size().Y)
 		})
 	}
 }
