ac32b564e39a0d9d58b505ca616135f253ecbeea
danikarik <baitursynov92@gmail.com>
2019-08-23 22:25:05 +0600

    wip

--- cmd/detect/main.go
+++ cmd/detect/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"image"
+	"image/color"
 	"image/png"
 	"log"
 	"os"
+	"sort"
 
 	"github.com/danikarik/mrz"
 	"gocv.io/x/gocv"
@@ -18,10 +20,11 @@ func main() {
 	dst := original.Clone()
 
 	dst = mrz.Resize(dst, 0, 600, gocv.InterpolationArea)
-	dst = mrz.Color(dst, gocv.ColorBGRToGray)
+	gocv.CvtColor(dst, &dst, gocv.ColorBGRToGray)
 
-	gocv.GaussianBlur(dst, &dst, image.Point{X: 3, Y: 3}, 0, 0, gocv.BorderConstant)
-	gocv.MorphologyEx(dst, &dst, gocv.MorphBlackhat, rectKernel)
+	gray := dst.Clone()
+	gocv.GaussianBlur(dst, &gray, image.Point{X: 3, Y: 3}, 0, 0, gocv.BorderConstant)
+	gocv.MorphologyEx(gray, &dst, gocv.MorphBlackhat, rectKernel)
 
 	gocv.Sobel(dst, &dst, 0, 1, 0, 1, 1, 0, gocv.BorderConstant)
 	gocv.ConvertScaleAbs(dst, &dst, 1, 0)
@@ -33,7 +36,65 @@ func main() {
 	m := gocv.NewMatWithSize(3, 3, gocv.MatTypeCV8U)
 	gocv.Erode(dst, &dst, m)
 
-	img, err := dst.ToImage()
+	// p = int(image.shape[1] * 0.05)
+	// thresh[:, 0:p] = 0
+	// thresh[:, image.shape[1] - p:] = 0
+	// TODO:
+	p := int(float64(dst.Size()[1]) * 0.05)
+	for i := 0; i < dst.Rows(); i++ {
+		for j := 0; j < dst.Cols(); j++ {
+			if j < p {
+				dst.SetUCharAt(i, j, 0)
+			}
+		}
+	}
+	for i := 0; i < dst.Rows(); i++ {
+		for j := 0; j < dst.Cols(); j++ {
+			if dst.Size()[1]-p <= j {
+				dst.SetUCharAt(i, j, 0)
+			}
+		}
+	}
+
+	// cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	// cnts = imutils.grab_contours(cnts)
+	// cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+	// TODO:
+	cnts := gocv.FindContours(dst, gocv.RetrievalExternal, gocv.ChainApproxSimple)
+	sort.SliceStable(cnts, func(i, j int) bool {
+		return gocv.ContourArea(cnts[i]) > gocv.ContourArea(cnts[j])
+	})
+
+	for _, c := range cnts {
+		rect := gocv.BoundingRect(c)
+		x, y, w, h := rect.Size().X, rect.Size().Y, rect.Dx(), rect.Dy()
+		ar := float64(w) / float64(h)
+		crWidth := float64(w) / float64(gray.Size()[1])
+
+		if ar > 5 && crWidth > 0.75 {
+			pX := int(float64(x+w) * 0.03)
+			pY := int(float64(y+h) * 0.03)
+			x, y = x-pX, y-pY
+			w, h = w+(pX*2), h+(pY*2)
+
+			// TODO:
+			// roi = image[y:y + h, x:x + w].copy()
+			green := image.Rectangle{
+				Min: image.Point{
+					X: x,
+					Y: y,
+				},
+				Max: image.Point{
+					X: x + w,
+					Y: y + h,
+				},
+			}
+			greenColor := color.RGBA{R: 0, G: 230, B: 64, A: 1}
+			gocv.Rectangle(&original, green, greenColor, 2)
+		}
+	}
+
+	img, err := original.ToImage()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,7 +109,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	window := gocv.NewWindow("Hello")
-	window.IMShow(dst)
-	window.WaitKey(0)
+	// window := gocv.NewWindow("Hello")
+	// window.IMShow(dst)
+	// window.WaitKey(0)
 }
--- mrz.go
+++ mrz.go
@@ -1,12 +1,8 @@
 package mrz
 
 import (
-	"bytes"
 	"errors"
 	"image"
-	"image/png"
-	"io/ioutil"
-	"os"
 
 	"gocv.io/x/gocv"
 )
@@ -37,52 +33,59 @@ func Resize(mat gocv.Mat, width, height float64, interp gocv.InterpolationFlags)
 	return mat
 }
 
-// Color converts color.
-func Color(mat gocv.Mat, code gocv.ColorConversionCode) gocv.Mat {
-	gocv.CvtColor(mat, &mat, code)
-	return mat
+// GrabContours returns points depends on OpenCV.
+func GrabContours(cnts [][]image.Point) ([]image.Point, error) {
+	if len(cnts) == 2 {
+		return cnts[0], nil
+	}
+	if len(cnts) == 3 {
+		return cnts[1], nil
+	}
+	return nil, errors.New("contours must have length 2 or 3")
 }
 
 // ScanImageFromBytes reads MRZ from given content.
 func ScanImageFromBytes(data []byte) ([]byte, error) {
-	file, err := ioutil.TempFile("", "mrz_")
-	if err != nil {
-		return nil, err
-	}
+	// file, err := ioutil.TempFile("", "mrz_")
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	_, err = file.Write(data)
-	if err != nil {
-		return nil, err
-	}
+	// _, err = file.Write(data)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	err = file.Close()
-	if err != nil {
-		return nil, err
-	}
+	// err = file.Close()
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	mat := gocv.IMRead(file.Name(), gocv.IMReadUnchanged)
-	mat = Resize(mat, 0, 600, gocv.InterpolationArea)
+	// mat := gocv.IMRead(file.Name(), gocv.IMReadUnchanged)
+	// mat = Resize(mat, 0, 600, gocv.InterpolationArea)
 
-	gray := Color(mat, gocv.ColorBGRToGray)
-	gocv.GaussianBlur(gray, &gray, image.Point{X: 3, Y: 3}, 0, 0, gocv.BorderConstant)
+	// gray := Color(mat, gocv.ColorBGRToGray)
 
-	img, err := mat.ToImage()
-	if err != nil {
-		return nil, err
-	}
+	// gocv.GaussianBlur(gray, &gray, image.Point{X: 3, Y: 3}, 0, 0, gocv.BorderConstant)
 
-	buf := new(bytes.Buffer)
-	err = png.Encode(buf, img)
-	if err != nil {
-		return nil, err
-	}
+	// img, err := mat.ToImage()
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	err = os.Remove(file.Name())
-	if err != nil {
-		return nil, err
-	}
+	// buf := new(bytes.Buffer)
+	// err = png.Encode(buf, img)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	return buf.Bytes(), nil
+	// err = os.Remove(file.Name())
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// return buf.Bytes(), nil
+	return nil, errors.New("not implemented yet")
 }
 
 // ScanImage reads MRZ from given source.
